177517c90359e4d30aefafb516efd073fd3de3ca
rks1337 <tudby001@mymail.unisa.edu.au>
2017-07-10 20:06:41 +0930

    Add files via upload

---
+++ andriod/src/bloc/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package bloc;
+
+import android.os.Bundle;
+import bloc.Boot;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Boot(), config);
+	}
+}
